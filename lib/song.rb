ba15fbb384c1e651db837603ea78fac43eb45a06
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-03 00:04:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,28 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+      song = self.new 
+      self.all << song 
+      song 
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name 
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    self.all << song 
+    song 
+  end
+  
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
 
 end
